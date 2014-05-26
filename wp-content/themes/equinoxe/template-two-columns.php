ada779baff5cf57470ed2697a37ccd800615b850
rku4er <rku4er@gmail.com>
2014-05-26 22:05:40 +0300

    bugfixes

---
+++ wp-content/themes/equinoxe/template-two-columns.php
@@ -0,0 +1,82 @@
+<?php /* Template Name: Two Columns */ get_header(); ?>
+
+	<?php if(get_field('show_slider') && have_rows('seats')) :?>
+
+	<section id="slider">
+
+		<?php while(have_rows('seats')): the_row();?>
+
+		<div class="slide">
+
+			<div class="heading">
+				<div class="inner">
+					<h1 class="title"><?php the_sub_field('title'); ?></h1>
+					<p class="text">
+						<span>
+							<?php the_sub_field('sub_title'); ?>
+						</span>
+					</p>
+				</div>
+			</div>
+
+			<?php
+			$thumb = wp_get_attachment_image_src( get_sub_field('image'), 'full');
+			if($thumb): ?>
+
+			<img src="<?php echo $thumb[0]; ?>" alt="background" class="ground">
+
+			<?php endif; ?>
+
+		</div>
+
+		<?php endwhile; ?>
+
+	</section>
+
+	<?php endif; ?>
+
+	<div id="content-holder" class="single-listing">
+
+		<div class="midwrapper group">
+
+			<h1 id="listing-title"><?php the_title(); ?></h1>
+
+			<?php get_sidebar(); ?>
+
+			<section id="content" class="content group">
+
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+					<!-- article -->
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+						<?php the_content(); ?>
+
+						<br class="clear">
+
+						<?php edit_post_link(); ?>
+
+					</article>
+					<!-- /article -->
+
+				<?php endwhile; ?>
+
+				<?php else: ?>
+
+					<!-- article -->
+					<article>
+
+						<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+					</article>
+					<!-- /article -->
+
+				<?php endif; ?>
+
+			</section>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
